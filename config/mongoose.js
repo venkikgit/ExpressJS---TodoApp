bfce0ec0274925f6d563d593938d79b62619e2b6
Venkateswarlu <104353852+venkikgit@users.noreply.github.com>
2022-06-24 13:07:58 +0530

    Update mongoose.js
    Cloud MongoDB Added

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
 // require the library
 const mongoose = require('mongoose');
 //connect to the database
-mongoose.connect('mongodb://localhost/todolist_app_db'); //Choose it by typing here
+// mongoose.connect('mongodb://localhost/todolist_app_db'); //Choose it by typing here
+mongoose.connect('mongodb+srv://venki47:QI4ZnYrNayDHpOun@cluster0.artl5tx.mongodb.net/?retryWrites=true&w=majority');
 // acquire the connection
 const db = mongoose.connection;
 // error
